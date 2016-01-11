192956b12a02641902f8faa1d1fdcff43a6378f3
Bartłomiej Szałach <onegrx@gmail.com>
2016-01-11 17:34:01 +0100

    Generate id for students' cards.

---
+++ student_card.py
@@ -0,0 +1,22 @@
+__author__ = 'onegrx'
+
+from random import randint
+
+
+class StudentCard:
+
+    def random_with_n_digits(self, n):
+        range_start = 10**(n-1)
+        range_end = (10**n)-1
+        return randint(range_start, range_end)
+
+    def generate(self, digits):
+        return self.random_with_n_digits(digits)
+
+    def show(self, n):
+        print(self.generate(n))
+
+
+for i in range(100):
+    gen = StudentCard()
+    gen.show(4)
