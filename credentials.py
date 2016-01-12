5de7ddb197d25fc522afe6c9745e5c268603c2f8
Bartłomiej Szałach <onegrx@gmail.com>
2016-01-12 01:05:14 +0100

    Login and password generator add.

---
+++ credentials.py
@@ -0,0 +1,17 @@
+__author__ = 'onegrx'
+
+import subprocess
+import sys
+
+
+class Credentials:
+
+    def gen_password(self):
+        password = subprocess.check_output(["openssl", "rand", "-hex", "6"]
+                                           ).decode(sys.stdout.encoding).strip()
+        return password
+
+    def gen_login(self, name, surname):
+        login = name[0:4] + surname[0:6]
+        return login.lower()
+
