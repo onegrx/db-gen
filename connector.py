5d3a5f53ac4d1056170affec7e25c4d3cd48d17c
Bartłomiej Szałach <onegrx@gmail.com>
2016-01-23 00:36:04 +0100

    Refactor connector.

--- connector.py
+++ connector.py
@@ -2,6 +2,8 @@ __author__ = 'onegrx'
 
 import pymssql
 
+from password import secretpassword
+
 class Connector:
 
     def __init__(self):
@@ -14,22 +16,29 @@ class Connector:
         conn.autocommit(True)
         return conn
 
-    """ Execue procedure on conn, args has to be tuple of arguments"""
-    def execproc(self, conn, procedure, args):
+    def apply_proc(self, procedure, args):
+        conn = self.connect(secretpassword)
         cursor = conn.cursor()
         cursor.callproc(procedure, args)
-
-    def execquery(self, conn, query):
-        cursor = conn.cursor()
-        cursor.execute(query)
-
-    def fetchdata(self, password):
-        connection = self.connect(password)
-        cursor = connection.cursor()
-        return cursor
-
-    def close(self, conn):
         conn.close()
+
+
+    # """ Execue procedure on conn, args has to be tuple of arguments"""
+    # def execproc(self, conn, procedure, args):
+    #     cursor = conn.cursor()
+    #     cursor.callproc(procedure, args)
+
+    # def execquery(self, conn, query):
+    #     cursor = conn.cursor()
+    #     cursor.execute(query)
+    #
+    # def fetchdata(self, password):
+    #     connection = self.connect(password)
+    #     cursor = connection.cursor()
+    #     return cursor
+
+
+
 
 
 
