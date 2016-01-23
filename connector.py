facbb3be5a635cfc3a6822d2ee6190b50dbefd0d
Bartłomiej Szałach <onegrx@gmail.com>
2016-01-23 03:16:50 +0100

    Allow executing multiple procedures on the connection.

--- connector.py
+++ connector.py
@@ -22,6 +22,14 @@ class Connector:
         cursor.callproc(procedure, args)
         conn.close()
 
+    def apply_proc_multi(self, procedure, args, n):
+        conn = self.connect(secretpassword)
+        cursor = conn.cursor()
+        for i in range(n):
+            cursor.callproc(procedure, args[i])
+            print(procedure + " " + ", ".join(str(v) for v in args[i]))
+        conn.close()
+
 
     # """ Execue procedure on conn, args has to be tuple of arguments"""
     # def execproc(self, conn, procedure, args):
