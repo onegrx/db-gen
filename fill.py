02b78ca3c0bd88091810245c088e2ad79e425b6e
Bartłomiej Szałach <onegrx@gmail.com>
2016-01-23 03:31:16 +0100

    Functions which fill the database using generators and stored procedures.

---
+++ fill.py
@@ -0,0 +1,42 @@
+__author__ = 'onegrx'
+
+from generator import *
+from connector import Connector
+
+filler = Connector()
+records = 10
+
+"""The first slow version of calling, one connection per one query"""
+def fill_add_company_slow():
+    for i in range(records):
+        proc = 'AddCompany'
+        args = gen_add_company()
+        print(proc + " " + ", ".join(str(arg) for arg in args))
+        filler.apply_proc(proc, args)
+
+"""The second and fast version of calling, one connection for all queries"""
+def fill_add_company():
+    procedure = 'AddCompany'
+    # any other way without i?
+    args = [gen_add_company() for i in range(records)]
+    filler.apply_proc_multi(procedure, args, records)
+
+
+def fill_add_conference_slow():
+    for i in range(records):
+        proc = 'AddConference'
+        args = gen_add_conference()
+        print(proc + " " + ", ".join(str(arg) for arg in args))
+        filler.apply_proc(proc, args)
+
+
+def fill_add_conference():
+    procedure = 'AddConference'
+    args = [gen_add_conference() for i in range(records)]
+    filler.apply_proc_multi(procedure, args, records)
+
+
+
+
+
+
