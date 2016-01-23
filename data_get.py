7e46c06729cc90c13f8597f9ff5c12f22e2c8851
Bartłomiej Szałach <onegrx@gmail.com>
2016-01-23 03:17:16 +0100

    Data getters functions.

--- data_get.py
+++ data_get.py
@@ -8,25 +8,47 @@ import generators.credentials
 import generators.company
 import generators.street
 import generators.conference
+import generators.person
+
+
+def getNameSurname():
+    personality = generators.person.Person()
+    name, surname = personality.generate()
+    return name, surname
+
 
 def getMail(user):
     mail = user[0:5].lower() + "@" + user[0:3] + ".com"
     return mail
+
+
+def getMailPersonal(n, s):
+    return n[0:6].lower() + "@" + s[0:5].lower + ".com"
+
 
 def getCompany():
     company_gen = generators.company.Company()
     company = company_gen.generate()
     return company
 
+
 def getCityCountry():
     location = generators.location.Location()
     city, country = location.generate()
     return city, country
 
-def getLogin(company):
+
+def getLoginCompany(company):
     credentials = generators.credentials.Credentials()
     login = credentials.gen_login_company(company)
     return login
+
+
+def getLoginPersonal(n, s):
+    credentials = generators.credentials.Credentials()
+    login = credentials.gen_login_personal(n, s)
+    return login
+
 
 def getPhone():
     range_start = 10**(9-1)
@@ -34,19 +56,28 @@ def getPhone():
     phone = random.randint(range_start, range_end)
     return phone
 
+
 def getStreet():
     street_gen = generators.street.Street()
     street = street_gen.generate()
     return street
 
+
 def getZip():
     zipcode = random.randint(10**4, 10**5 - 1)
     return zipcode
+
+
+def getNip():
+    nip = random.randint(10**9, 10**10 - 1)
+    return str(nip)
+
 
 def getConference():
     conference_gen = generators.conference.Conference()
     conference = conference_gen.generate()
     return conference
+
 
 def getDates():
     currentdate = dt.datetime.now()
@@ -57,3 +88,7 @@ def getDates():
     start = (str(begin.year), str(begin.month), str(begin.day))
     end = (str(end.year), str(end.month), str(end.day))
     return "/".join(start), "/".join(end)
+
+
+def getPrice():
+    return random.randint(3, 500) * 10
